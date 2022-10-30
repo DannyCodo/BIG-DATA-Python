3360aef6670a081d972473320138b8b9c4d2b099
DannyCodo <104183363+DannyCodo@users.noreply.github.com>
2022-10-30 04:06:08 -0300

    Update python_pruebas_01.py
    pruebas de identidad de un objeto de clase int

--- python_pruebas_01.py
+++ python_pruebas_01.py
@@ -12,4 +12,8 @@ x = 5
 print(id(x)) # es otro objeto (Ha cambiado)
 
 x = x + 1
+print(id(x)) # otro objeto (Ha cambiado pero detectó que es 6 y volvió a apuntar al objeto: 6)
+
+x = x + 1
 print(id(x)) # otro objeto (Ha cambiado)
+
