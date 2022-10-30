ed703e7dbb48c575909651297223e5ce29ed5c22
DannyCodo <104183363+DannyCodo@users.noreply.github.com>
2022-10-30 03:55:43 -0300

    Update python_pruebas_01.py

--- python_pruebas_01.py
+++ python_pruebas_01.py
@@ -3,10 +3,13 @@ x = 5
 print(id(x)) # un objeto
 
 x = 5
-print(id(x)) # un objeto
+print(id(x)) # es el mismo objeto
 
 x = 6
-print(id(x)) # un objeto
+print(id(x)) # otro objeto (Ha cambiado)
+
+x = 5
+print(id(x)) # es otro objeto (Ha cambiado)
 
 x = x + 1
 print(id(x)) # otro objeto (Ha cambiado)
